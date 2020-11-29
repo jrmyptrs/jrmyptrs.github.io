c1167f2c37b90c121ef907d2e50462721891fa16
Jeremy Peters <jrmyptrs@gmail.com>
2020-11-29 18:42:30 -0500

    hiding touch test

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var figure = $(".video-hover").hover( hoverVideo, hideVideo );
 
-  $(".video-hover").live("touchstart", function(e) {
-    $(this).trigger("hover");
-  });
-
-  $(".video-hover").live("touchend", function(e) {
-    $(this).trigger("blur");
-  });
+  // $(".video-hover").live("touchstart", function(e) {
+  //   $(this).trigger("hover");
+  // });
+  //
+  // $(".video-hover").live("touchend", function(e) {
+  //   $(this).trigger("blur");
+  // });
 
   function hoverVideo(e) {
     $('.jp-bgvideo').removeClass('playing');
