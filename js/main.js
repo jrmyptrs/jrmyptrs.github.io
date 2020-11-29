be22307a2bf421ff4bdfc0937969d9016630d6e1
Jeremy Peters <jrmyptrs@gmail.com>
2020-11-29 18:20:31 -0500

    testing touch support

--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var figure = $(".video-hover").hover( hoverVideo, hideVideo );
 
+  $(".video-hover").live("touchstart", function(e) {
+    $(this).trigger("hover");
+  });
+
+  $(".video-hover").live("touchend", function(e) {
+    $(this).trigger("blur");
+  });
+
   function hoverVideo(e) {
     $('.jp-bgvideo').removeClass('playing');
     let currentVideo = $(this).data("hover");
