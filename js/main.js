4d75ea73a03080b3d8aa3937d9dc69964a3b06f9
JP <jrmyptrs@gmail.com>
2020-12-01 11:18:02 -0500

    video credit

--- js/main.js
+++ js/main.js
@@ -3,26 +3,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var figure = $(".video-hover").hover( hoverVideo, hideVideo );
 
-  // $(".video-hover").live("touchstart", function(e) {
-  //   $(this).trigger("hover");
-  // });
-  //
-  // $(".video-hover").live("touchend", function(e) {
-  //   $(this).trigger("blur");
-  // });
-
   function hoverVideo(e) {
     $('.jp-bgvideo').removeClass('playing');
     let currentVideo = $(this).data("hover");
     $('#'+currentVideo).addClass('active').get(0).play();
     $('.jp-bgvideo').addClass('playing');
     $('.jp-intro').addClass('white');
+    if(currentVideo === 'hover-wave') { } else {
+      $(".jp-bgvideo__credit").slideDown('fast');
+    }
   }
 
   function hideVideo(e) {
     let currentVideo = $(this).data("hover");
     $('#'+currentVideo).removeClass('active').get(0).pause();
+    if(currentVideo === 'hover-wave') { } else {
+      $(".jp-bgvideo__credit").slideUp('fast');
+    }
     $('.jp-intro').removeClass('white');
     $('.jp-bgvideo').removeClass('playing');
   }
+
 });
